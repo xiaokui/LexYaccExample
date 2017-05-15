4f57c183fc733219a3bd71083553d9eeae9c6dc0
xiaokui <xiaokui.zhao@163.com>
2017-05-15 14:34:30 +0800

    提交 计算器 支持单个字母

---
+++ CalcExample2/main.cpp
@@ -0,0 +1,27 @@
+#include <QtCore>
+#include <stdio.h>
+#include <qfile.h>
+#include <QByteArray>
+#include <QTextCodec>
+
+#include "lexer.h"
+#include "parser.h"
+
+extern FILE *yyin;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    yyin = fopen("00.txt", "r");
+
+    //yylex();
+    yyparse();
+
+    fclose(yyin);
+
+    return app.exec();
+}
+
+
+
