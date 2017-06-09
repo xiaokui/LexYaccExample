1aa5016fb8e158fa440971577dced9954861d5f2
xiaokui <xiaokui.zhao@163.com>
2017-06-09 15:45:15 +0800

    添加mgl的初始工程

---
+++ MGLExample/main.cpp
@@ -0,0 +1,32 @@
+#include <QtCore>
+#include <stdio.h>
+#include <qfile.h>
+#include <QByteArray>
+#include <QTextCodec>
+
+#include "lexer.h"
+#include "parser.h"
+#include "SymbolTable.h"
+
+extern FILE *yyin;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    addfunc("sqrt", sqrt);
+    addfunc("log", log);
+
+
+    yyin = fopen("00.txt", "r");
+
+    //yylex();
+    yyparse();
+
+    fclose(yyin);
+
+    return app.exec();
+}
+
+
+
